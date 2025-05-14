caf5fb789668fc797f5218600c830c38bf1fe08b
Dark <patriciastan99@gmail.com>
2025-05-14 10:46:57 -0400

    movement 1

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -1,5 +1,31 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Boid : MonoBehaviour
 {
+    public Rigidbody rigidbody;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.linearVelocity = Random.insideUnitSphere;
+    }
+
+
+    private void Update()
+    {
+
+        AlignToVelocity();
+
+    }
+
+    public void AlignToVelocity()
+    {
+        Vector3 velocity = rigidbody.linearVelocity;
+        Vector3 forward = transform.forward;
+        float angle = Vector3.SignedAngle(forward, velocity, Vector3.up);
+        Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 5f);
+
+
+    }
 }
